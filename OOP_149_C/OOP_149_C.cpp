31c4669f6f672e94c3f26995e27c14df4136ab4c
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-04-10 10:40:09 +0700

    membuat method input

--- OOP_149_C/OOP_149_C.cpp
+++ OOP_149_C/OOP_149_C.cpp
@@ -14,8 +14,16 @@ public: void cetak() {
 
 class matakuliah {
 private:
-	string nim;
-	string nama;
+	string kode;
+	string namaM;
 	int sks;
 public:
+	void input() {
+		cout << " Masukkan Kode Mata Kuliah : ";
+		cin >> kode;
+		cout << " Masukkan Nama Mata KUliah : ";
+		cin >> namaM;
+		cout << " Masukkan SKS Mata Kuliah : ";
+		cin >> sks;
+	}
 };
