af4f87e027c9d3ec17b1e3f099cb8a1eed86acd5
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-04-10 10:32:01 +0700

    Add project files.

---
+++ OOP_149_C/OOP_149_C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+class mahasiswa {
+public:
+	string nim;
+	string nama;
+public: void cetak() {
+	cout << " NIM = " << nim;
+	cout << "\nNama = " << nama;
+	}
+};
